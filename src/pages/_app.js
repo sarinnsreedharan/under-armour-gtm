5bf9ef297852d32b89b3bb0eac13e7b8361ce050
Sarin N. Sreedharan <sarin@COK-M-00164.local>
2024-09-24 17:31:41 +0530

    script tag added in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,33 @@
 import "@/styles/globals.css";
 import useGTM from "@/hooks/useGTM";
+import { useEffect } from 'react';
+import Script from 'next/script';
+
 
 export default function App({ Component, pageProps }) {
-  useGTM();
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    // GTM Initialization
+    const handleGTM = () => {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: 'gtm.js',
+        'gtm.start': new Date().getTime(),
+      });
+    };
+    handleGTM();
+  }, []);
+  return (<>
+        <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MT6Q2N3B');`,
+        }}
+      />
+  <Component {...pageProps} />
+  </>)
 }
