97559db4c5698078f2224684dcb3688c8330672e
Sarin N. Sreedharan <sarin@COK-M-00164.local>
2024-09-24 17:10:29 +0530

    GTM tag uncommented

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,5 +2,6 @@ import "@/styles/globals.css";
 import useGTM from "@/hooks/useGTM";
 
 export default function App({ Component, pageProps }) {
+  useGTM();
   return <Component {...pageProps} />;
 }
