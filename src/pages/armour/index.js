0c3db94c715ca854e5d79ebb19e16663de67a540
Sarin N. Sreedharan <sarin@COK-M-00164.local>
2024-09-27 18:35:34 +0530

    button click event added

--- src/pages/armour/index.js
+++ src/pages/armour/index.js
@@ -8,10 +8,24 @@ export default function Armour() {
     window.dataLayer = window.dataLayer || [];
     window.dataLayer.push({
       event: "pageView",
-      page_path: 'sarin'
+      page_path: "sarin",
     });
   }, []);
   const armourButtonClicked = () => {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: "gtm.click",
+      gtm: {
+        uniqueEventId: 10,
+        start: 1727441896509,
+        element: "Just a normal dummy element",
+        elementClasses: "987",
+        elementId: "1234",
+        elementTarget: "",
+        triggers: "2",
+        elementUrl: "https:sarin",
+      },
+    });
     setButtonClicked(!buttonClicked);
   };
 
