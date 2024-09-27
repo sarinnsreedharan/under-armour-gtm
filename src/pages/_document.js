0a8d4904a047881c9e4dff0ef102b1b3a1093ed8
Sarin N. Sreedharan <sarin@COK-M-00164.local>
2024-09-27 17:10:47 +0530

    pageView event added in armour button click

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { GoogleTagManager } from "@next/third-parties/google";
+// import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function Document() {
   return (
@@ -17,18 +17,15 @@ export default function Document() {
           }}
         />
       </Head>
-      <GoogleTagManager gtmId="GTM-MT6Q2N3B" />
+      {/* <GoogleTagManager gtmId="GTM-MT6Q2N3B" /> */}
       <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MT6Q2N3B"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
         <Main />
         <NextScript />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MT6Q2N3B" height="0" width="0" style="display: none; visibility: hidden;" />`,
+          }}
+        />
       </body>
     </Html>
   );
--- src/pages/armour/index.js
+++ src/pages/armour/index.js
@@ -4,11 +4,20 @@ export default function Armour() {
 
     const[textValue,setTextValue]=useState('');
     const[buttonClicked,setButtonClicked]=useState(false);
+    const armourButtonClicked=()=>{
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            event: 'pageView',
+            page_path: '/new-page-url',  // Custom URL path
+            page_title: 'New Page Title'  // Custom page title
+          });
+        setButtonClicked(!buttonClicked)
+    }
 
     return(
         <>
         <input type="text" value={textValue}  onChange={(e)=>{setTextValue(e.target.value)}}/>
-        <button onClick={()=>setButtonClicked(!buttonClicked)}>Click me!</button>
+        <button onClick={armourButtonClicked}>Click me!</button>
         {buttonClicked && <><h2>The word you have entered is:</h2><span>{textValue}</span></>}
         </>
     )
